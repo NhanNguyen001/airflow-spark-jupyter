3c9965c5abe01fbd7ca94e439520a5d7b9faa19f
Nhan Nguyen <nhannt22@hdbank.com.vn>
2024-09-09 22:39:51 +0700

    update code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,29 @@
+# # Start Jupyter Lab in the background
+# jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+
+# # Run the command passed to docker run
+# exec "$@"
+
 #!/bin/bash
 set -e
 
-# Start Jupyter Lab in the background
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+# Initialize Airflow database if it hasn't been initialized
+airflow db init
+
+# Create admin user if it doesn't exist
+airflow users list | grep -q admin || \
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+
+# Start Jupyter Lab in the background if the command is jupyter
+if [ "$1" = "jupyter" ]; then
+  jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+fi
 
 # Run the command passed to docker run
 exec "$@"
